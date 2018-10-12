c0b636b3d5fe875d097ef97d645e2606cdbb9baf
lucas-sigma <lucasn.sigma@gmail.com>
2018-10-12 16:05:19 -0300

    add task2 with comments

--- Projetos/Proj-01/app.py
+++ Projetos/Proj-01/app.py
@@ -1,15 +1,28 @@
-def leitura():
-    try:
-        data = open('usuarios.txt', 'r')
-        print(data)
-    except IOError:
+'''
+Lendo dados e armazenando na variavel data. Apos isso, eh criada uma variavel listData para criar uma lista com os dados. listData tem seus dados separados por um split.
+'''
+
+try:
+    data = open('usuarios.txt', 'r')
+    listData = []
+    listData.append(data.read())
+    listData = [i.split('\n') for i in listData]
+    data.close()
+
+    # test
+    for i in listData:
+        if isinstance(i, list):
+            for k in i:
+                print(k)
+    
+except IOError:
         print('arquivo nao encontrado!')
 
-def formatoRelatorio(arr):
+'''def formatoRelatorio(arr):
     print('ACME Inc.           Uso do espaço em disco pelos usuários')
     print('------------------------------------------------------------------------')
     print(r'Nr.  Usuário        Espaço utilizado     % do uso\n')
     for i in len(arr):
         print(identificador, '    ', nome, '       ', espacoUtilizado, '            ', percentUso)
     print(f'\nEspaço total ocupado: {totalOcupado} MB')
-    print(f'Espaço médio ocupado: {medioOcupado} MB')
+    print(f'Espaço médio ocupado: {medioOcupado} MB')'''
