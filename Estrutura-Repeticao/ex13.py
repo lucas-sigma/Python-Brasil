be7af9ad41fb82a195668fc4cc1ab636666bb0d8
lucas-sigma <lucasn.sigma@gmail.com>
2018-11-20 18:10:27 -0200

    add ex13

---
+++ Estrutura-Repeticao/ex13.py
@@ -0,0 +1,8 @@
+'''
+Faça um programa que peça dois números, base e expoente, calcule e mostre o primeiro número elevado ao segundo número. Não utilize a função de potência da linguagem.
+'''
+
+x1, x2 = int(input('Base: ')), int(input('Expoente: '))
+x3 = x1 ** x2
+
+print(x3)
