07bd96d3e155f067844c0b3080cd4944ce8826df
lucas-sigma <lucasn.sigma@gmail.com>
2018-11-21 18:26:27 -0200

    add ex15

--- Estrutura-Repeticao/ex15.py
+++ Estrutura-Repeticao/ex15.py
@@ -3,6 +3,10 @@ A série de Fibonacci é formada pela seqüência 1,1,2,3,5,8,13,21,34,55,... Fa
 '''
 
 n = int(input('N: '))
+n1, n2 = 0, 1
 
-for i in range(1, n, n+(n-1)):
-    print(i)
+for i in range(n):
+    print(n1)
+    print(n2)
+    n1 += n2
+    n2 += n1
